6538e6587ccf32062199442945c615600e5acf3d
Charlene Kwok <charlenekwok499@gmail.com>
2022-07-03 16:12:11 +1000

    added router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,30 @@
 // import classes from "./App.module.scss";
+import { Fragment } from "react";
+import { Route, Switch } from "react-router-dom";
+import NotFound from "./components/404Page/NotFound";
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/Signup";
+import Resources from "./components/educational/Resources";
+import Forum from "./components/forum/Forum";
+import Home from "./components/home/Home";
+import JobList from "./components/home/JobList";
+import NavBar from "./components/home/NavBar";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Fragment>
+      <NavBar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/job" exact component={JobList} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/signup" exact component={Signup} />
+        <Route path="/forum" exact component={Forum} />
+        <Route path="/resources" exact component={Resources} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </Fragment>
+  );
 }
 
 export default App;
