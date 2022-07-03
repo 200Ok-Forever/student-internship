9137116e4006838cb0a7fd320000a7f3609324c3
Charlene Kwok <charlenekwok499@gmail.com>
2022-07-03 17:06:33 +1000

    added app bar and 404 not found page

--- client/src/components/404Page/NotFound.js
+++ client/src/components/404Page/NotFound.js
@@ -1,17 +1,20 @@
 import { Button } from "@mui/material";
 import React from "react";
+import { useHistory } from "react-router-dom";
 import classes from "./NotFound.module.scss";
 
 const NotFound = () => {
+  const history = useHistory();
+
   return (
     <div className={classes.container}>
-      <img
-        src="https://i.imgur.com/Q2BAOd2.png"
-        alt="404 not found"
-        width="400px"
-      />
+      <img src="https://i.imgur.com/Q2BAOd2.png" alt="404 not found" />
       <h1>OOPS! PAGE NOT FOUND!</h1>
-      <Button variant="outlined" color="primary">
+      <Button
+        variant="outlined"
+        color="secondary"
+        onClick={() => history.push("/")}
+      >
         Go to homepage
       </Button>
     </div>
