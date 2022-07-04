c4df132e7a33c93218afd9b3f3ad0e9eb9742ff5
Lilian Wang <lilian.wang1712@gmail.com>
2022-07-04 12:06:19 +1000

    root container

--- client/src/App.js
+++ client/src/App.js
@@ -9,20 +9,24 @@ import Forum from "./components/forum/Forum";
 import Home from "./components/home/Home";
 import JobList from "./components/home/JobList";
 import NavBar from "./components/appBar/NavBar";
+import { Container } from '@mui/material';
+import classes from "./App.module.scss";
 
 function App() {
   return (
     <Fragment>
       <NavBar />
-      <Switch>
-        <Route path="/" exact component={Home}></Route>
-        <Route path="/job" exact component={JobList} />
-        <Route path="/login" exact component={Login} />
-        <Route path="/signup" exact component={Signup} />
-        <Route path="/forum" exact component={Forum} />
-        <Route path="/resources" exact component={Resources} />
-        <Route path="*" component={NotFound} />
-      </Switch>
+      <Container maxWidth="xl" className={classes.rootContainer}>
+        <Switch>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/job" exact component={JobList} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/signup" exact component={Signup} />
+          <Route path="/forum" exact component={Forum} />
+          <Route path="/resources" exact component={Resources} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </Container>
     </Fragment>
   );
 }
