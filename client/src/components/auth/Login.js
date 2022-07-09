0c05a53e2e967d3ea3c41058e4332be140783307
Yilang (Leon) Wu <yilangwu@outlook.com>
2022-07-09 17:14:52 +1000

    implement login and sign up form

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,106 @@
 import React from "react";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { ThemeProvider } from "@mui/material/styles";
+import { useHistory } from "react-router-dom";
+import theme from "../UI/theme";
+import { Paper } from "@mui/material";
 
 const Login = () => {
-  return <div>Login</div>;
+  const history = useHistory();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs" sx={{height: "100vh"}}>
+        <CssBaseline />
+        <Paper elevation={10}>
+          <Box
+            sx={{
+              marginTop: 16,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              padding: "36px",
+            }}
+          >
+            <Typography
+              component="h1"
+              variant="h4"
+              fontWeight="bold"
+              sx={{ textAlign: "center" }}
+            >
+              Sign in
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              noValidate
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <Grid container>
+                <Link href="#" variant="body2" onClick={() => history.push("/signup")}>
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Log In
+              </Button>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ margin: "auto" }}
+                onClick={() => history.push("/signup")}
+              >
+                Sign Up
+              </Button>
+            </Box>
+          </Box>
+        </Paper>
+      </Container>
+    </ThemeProvider>
+  );
 };
 
 export default Login;
--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,7 +1,54 @@
 import React from "react";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import BusinessIcon from "@mui/icons-material/Business";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { ThemeProvider } from "@mui/material/styles";
+import SchoolIcon from "@mui/icons-material/School";
+import { Paper } from "@mui/material";
+import theme from "../UI/theme";
 
-const Signup = () => {
-  return <div>Signup</div>;
-};
+export default function SignUp() {
 
-export default Signup;
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Paper elevation={10}>
+        <Box
+          sx={{
+            marginTop: 16,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            padding: 6,
+          }}
+        >
+          <Typography component="h1" variant="h4" fontWeight="bold">
+            Sign up
+          </Typography>
+          <Box sx={{ mt: 3 }}>
+            <Grid container spacing={12}>
+              <Grid item xs={12} sm={6}>
+                <Button>
+                  <SchoolIcon fontSize="large" />
+                </Button>
+                <Typography fontWeight="bold">Student</Typography>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Button>
+                  <BusinessIcon fontSize="large"/>
+                </Button>
+                <Typography fontWeight="bold">Recruiter</Typography>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        </Paper>
+      </Container>
+    </ThemeProvider>
+  );
+}
