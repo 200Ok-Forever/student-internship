850ebd94ea5145362d601b7cda31533d228ea151
Charlene Kwok <charlenekwok499@gmail.com>
2022-07-10 13:20:39 +1000

    add job block

--- client/src/components/jobs/JobBlock.js
+++ client/src/components/jobs/JobBlock.js
@@ -1,5 +1,8 @@
-import { Paper } from "@mui/material";
+import { Avatar, Box, Paper, Typography } from "@mui/material";
 import React from "react";
+import salary from "../../asset/salary.png";
+import Label from "../UI/Label";
+import FmdGoodIcon from "@mui/icons-material/FmdGood";
 
 const JobBlock = ({ job }) => {
   // console.log(job);
@@ -7,14 +10,81 @@ const JobBlock = ({ job }) => {
     <Paper
       elevation={3}
       sx={{
-        width: "80%",
-        minWidth: "430px",
+        width: "100%",
         maxWidth: "1200px",
-        height: "200px",
-        p: "10px",
+        height: "240px",
+        p: "30px",
+        display: "flex",
+        flexDirection: "column",
+        rowGap: "10px",
       }}
     >
-      {job.title}
+      <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Typography
+          variant="h6"
+          gutterBottom
+          component="div"
+          fontFamily="inherit"
+          fontWeight="700"
+        >
+          {job.title}
+        </Typography>
+        {job?.status === "NEW" && (
+          <Typography
+            variant="h7"
+            gutterBottom
+            component="div"
+            fontFamily="inherit"
+            fontWeight="700"
+            color="primary"
+          >
+            {job.status}
+          </Typography>
+        )}
+      </Box>
+      <Box
+        sx={{ display: "flex", alignItems: "flex-start", columnGap: "10px" }}
+      >
+        <Avatar
+          src={job.company_avatar}
+          sx={{ width: 24, height: 24 }}
+        ></Avatar>
+        <Typography
+          variant="h8"
+          gutterBottom
+          component="div"
+          fontFamily="inherit"
+          fontWeight="700"
+        >
+          {job.company_name}
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "flex-start",
+            columnGap: "3px",
+            ml: "10px",
+          }}
+        >
+          <FmdGoodIcon fontSize="small" color="primary" />
+          {job.city}
+        </Box>
+      </Box>
+      <Box sx={{ display: "flex", columnGap: "14px" }}>
+        <Label text={job.min_salary + " - " + job.max_salary}>
+          <img src={salary} alt="salary" width="25px" height="25px" />
+        </Label>
+        <Label text={job.job_type}></Label>
+      </Box>
+      <Typography
+        variant="body1"
+        gutterBottom
+        component="div"
+        fontFamily="inherit"
+        sx={{ overflow: "hidden" }}
+      >
+        {job.description}
+      </Typography>
     </Paper>
   );
 };
