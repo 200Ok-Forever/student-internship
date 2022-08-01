9c2281a510703af7c82a08c8d2668ae46d203259
Yilang (Leon) Wu <yilangwu@outlook.com>
2022-08-02 03:13:28 +1000

    student user info page implemented

--- client/src/components/student/Profile.js
+++ client/src/components/student/Profile.js
@@ -1,4 +1,4 @@
-import { Paper, Typography } from "@mui/material";
+import { Paper, Typography, Avatar } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 import TitleWithIcon from "../UI/TitleWithIcon";
@@ -7,6 +7,7 @@ import JobBasicCard from "../UI/JobBasicCard";
 import CheckIcon from "@mui/icons-material/Check";
 import CloseIcon from "@mui/icons-material/Close";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import ProfileInfo from "./ProfileInfo";
 const data = [
   {
     title: "Software Engineer Intern",
@@ -40,10 +41,36 @@ const data = [
   },
 ];
 
+const infoData = {
+  first_name: "Leon",
+  last_name: "Wu",
+  university: "UNSW",
+  degree: "Bachelor of Computer Science",
+  positions: "Software Engineer, IT consultant",
+  major: "AI",
+  skills: "Java,C",
+  description:
+    "Hello! asdkljfaskldjfaskldjfaskldjfaklsdfjaskldfjaskldjfalskdfjasklfklasdjfklasf",
+};
+
 const Profile = () => {
   return (
-    <Box>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "35px"
+      }}
+    >
+      <Avatar sx={{ m: 1, bgcolor: "primary.main" }} />
+      <Typography variant="h4" fontWeight="bold" fontFamily="inherit">
+        {infoData.first_name} {infoData.last_name}
+      </Typography>
+      <ProfileInfo data={infoData}/>
       <TitleWithIcon
+        sx={{}}
         icon={<WorkHistoryIcon color="primary" />}
         text="Applied Internships"
       />
