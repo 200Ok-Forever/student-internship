5b55a950042dad1b5dd6e5ba3a57955519c1ece9
Charlene Kwok <charlenekwok499@gmail.com>
2022-08-05 01:47:13 +1000

    remove unused var

--- client/src/components/educational/GenerateResume.js
+++ client/src/components/educational/GenerateResume.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { ResumeInfoContext } from "../../store/ResumeInfoContext";
 
 const GenerateResume = () => {
-  const { allInfo, setAllInfo } = useContext(ResumeInfoContext);
+  const { allInfo } = useContext(ResumeInfoContext);
   console.log("🚀 ~ allInfo", allInfo);
 
   return <div>GenerateResume</div>;
