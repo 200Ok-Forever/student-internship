d6a68c6236ae388dd3641998869ae8c1d9cb7ad7
lavender <lavender@lavenderdeMacBook-Pro.local>
2022-08-06 21:51:07 +0800

    fix

--- server/app/Models/model.py
+++ server/app/Models/model.py
@@ -111,16 +111,7 @@ class Company(db.Model):
             "company_logo": self.company_logo
         }
 
-class JobSkills(db.Model):
-    __tablename__ = 'r_job_skill'
-    job_id = db.Column('job_id', db.Integer, db.ForeignKey('t_internships.id'), primary_key=True)
-    skill_id = db.Column('skill_id', db.Integer, db.ForeignKey('t_skills.id'), primary_key=True)
 
-
-class StudentSkills(db.Model):
-    __tablename__ = 'r_student_skill'
-    student_id = db.Column('student_id', db.Integer, db.ForeignKey('t_student.id'), primary_key=True)
-    skill_id = db.Column('skill_id', db.Integer, db.ForeignKey('t_skills.id'), primary_key=True)
 
 
 class Internship(db.Model):
@@ -233,19 +224,6 @@ class Comment(db.Model):
         }
 
 
-class Skill(db.Model):
-    __tablename__ = 't_skills'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.VARCHAR(255))
-    internships = db.relationship('Internship', secondary='r_job_skill', back_populates='skills', lazy=True)
-    students = db.relationship('Student', secondary='r_student_skill', back_populates='skills', lazy=True)
-    # students = db.relationship('Student', secondary='r_student_skill', back_populates='skills', lazy=True)
-    def get_info(self):
-        return {
-            "id": self.id,
-            "name": self.name
-        }
 
 
 class City(db.Model):
