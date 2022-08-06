b44bdfe5785317f824dc0e0df5d117750fd1fb9a
lavender <lavender@lavenderdeMacBook-Pro.local>
2022-08-06 22:13:02 +0800

    fix

--- server/app/APIs/Internships/internships_utils.py
+++ server/app/APIs/Internships/internships_utils.py
@@ -7,7 +7,8 @@ from json import dumps
 from requests import session
 from sqlalchemy import null
 from torch import is_same_size
-from ...Models.model import Calendar, StudentSkills,Internship, City, Company, Comment, User, Skill,InternshipStatus,Student, File,StudentInterveiwQuestion
+from ...Models.model import Calendar, Internship, City, Company, Comment, User, InternshipStatus,Student, File,StudentInterveiwQuestion
+from ...Models.skill import StudentSkills, Skill
 from flask_restx import Resource, reqparse
 from ...extension import db
 from string import digits
