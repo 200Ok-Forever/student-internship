292a5fd98de183b591d8258d32d1d0c4b37f6833
Haiyun Zhou <zhy1998618@163.com>
2022-08-07 10:28:12 +0800

    delete useless file

--- server/app/Helpers/helper.py
+++
@@ -1,9 +0,0 @@
-from ..Models.model import User, Student
-from ..Models.company import Companies, Industry
-import json
-from .. import db, mail, jwt
-
-
-
-def signup_student_quick(data):
-    pass
