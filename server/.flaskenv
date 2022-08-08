e7c7212802c6c28e5940034726c6f644277a5ce3
Yiwen Chen <z5241339@ad.unsw.edu.au>
2022-08-08 13:36:17 +0800

    delete push

--- server/.flaskenv
+++
@@ -1,3 +0,0 @@
-FLASK_APP = internHub.py
-FLASK_ENV = development
-FLASK_RUN_PORT = 5004
--- server/app/Helpers/other_util.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import inspect
-
-def convert_object_to_dict(obj):
-  return {str(c.key): getattr(obj, c.key) for c in inspect(obj).mapper.column_attrs}
-
-def convert_model_to_dict(rows):
-  return [convert_object_to_dict(row) for row in rows]
